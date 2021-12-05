0330016ff3105a6fb86e4f6d734b29ba91dd81b6
serkan <serkandemirel0420@gmail.com>
2021-12-05 08:48:19 -0800

    initial

---
+++ src/components/Search.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String
+})
+
+const count = ref(0)
+
+</script>
+
+
+
+
+
+
+
+
+<template>
+  <!-- <h1>{{ msg }}</h1> -->
+
+
+  <input id="search" type="text" v-model="search" @keyup.enter="search" />
+  <button @click="search">Search</button>
+
+  <!-- <button type="button" @click="count++">Count is  : {{ count }}</button> -->
+   
+</template>
+
+
+
+
+
+
+
+<style scoped>
+#search {
+  width: 80%;
+  height: 30px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 9px;
+  font-size: 22px;
+}
+
+a {
+  color: #42b983;
+}
+
+button {
+  background: #42b983;
+  color: #fff;
+  border: 0;
+  padding: 12px;
+  font-size: 22px;
+  cursor: pointer;
+   
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import { globalShortcut, tauri, app, window } from '@tauri-apps/api'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// app.getName().then(alert);
+
+
+let wm = new window.WindowManager()
+wm.setFocus()
+
+globalShortcut.register('CommandOrControl+K', () => {
+
+   //alert('CommandOrControl+Alt+K')
+    wm.setFocus()
+
+})
+
+ 
+createApp(App).mount('#app')
